a6da65937e0146522ea596870aa3f929c4325418
Adrian Cortes <ls-adrian@hotmail.com>
2021-11-14 18:14:03 -0500

    We add interaction to the New arraivals section with swiper.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -46,3 +46,11 @@ function scrollHeader(){
    if(this.scrollY >= 50) header.classList.add('scroll-header'); else header.classList.remove('scroll-header')
 }
 window.addEventListener('scroll', scrollHeader)
+
+/*=============== NEW SWIPER ===============*/
+let newSwiper = new Swiper(".new-swiper", {
+   centeredSlides: true,
+   slidesPerView: "auto",
+   loop: 'true',
+   spaceBetween: 16,
+});
